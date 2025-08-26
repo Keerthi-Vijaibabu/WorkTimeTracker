032b8ad0c91181e4165e8db1e1bc1e7153637e82
Keerthi Vijaibabu <keerthi.vijaibabu@gmail.com>
2025-08-26 14:37:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -165,8 +165,7 @@ export const addUserSession = async (uid: string, session: Omit<UserSession, 'id
 
 // Verification Log
 export const getVerificationLog = (callback: (logs: VerificationLogEntry[]) => void) => {
-    const logCol = collectionGroup(db, 'verificationLog');
-    const q = query(logCol, orderBy('timestamp', 'desc'));
+    const q = query(collectionGroup(db, 'verificationLog'), orderBy('timestamp', 'desc'));
     
     return onSnapshot(q, (querySnapshot) => {
         const logs: VerificationLogEntry[] = [];

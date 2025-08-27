427da042c4731e6e99a62fc8b60814f2ecbc8d3f
Keerthi Vijaibabu <keerthi.vijaibabu@gmail.com>
2025-08-27 03:46:56 +0000

    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsby

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 
 import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
+import Script from "next/script";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { AuthProvider } from "@/hooks/use-auth";
@@ -28,6 +29,12 @@ export default function RootLayout({
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
+        />
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5967409878775114"
+          crossOrigin="anonymous"
+          strategy="lazyOnload"
         />
       </head>
       <body className="font-body antialiased h-full" suppressHydrationWarning>
